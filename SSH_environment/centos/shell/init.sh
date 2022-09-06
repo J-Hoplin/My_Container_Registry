4cd2643671d094002d19cb65aa0d5e5af6582e08
J-hoplin1 <jhoplin7259@gmail.com>
2022-09-07 00:28:50 +0900

    feat(SSH_environment/centos) : seperate with dockerfile and init.sh

---
+++ SSH_environment/centos/shell/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install firewall and enable firewall
+yum install firewalld -y
+systemctl start firewalld
+systemctl enable firewalld
+
+# Install sshd
+yum install -y openssh-server
+
+# Add Port 22 to sshd_config file
+RUN echo "Port 22" >> /etc/ssh/sshd_config
+
+# Allow ssh port from firewall and reload firewall
+firewall-cmd --permanent --zone=public --add-port=22/tcp
+firewall-cmd --reload
+
+# Restart SSH
+RUN systemctl restart sshd
