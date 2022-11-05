9e2d9bb59f12aa818cd920cebc6cc26bfae946c8
andrewyoon10 <jhyoon0815103@gmail.com>
2022-11-05 21:20:48 +0900

    feat(centos) : add python script for initialization

---
+++ SSH_environment/centos/Readme.md
@@ -0,0 +1,30 @@
+SSH Environment : Cent OS Base
+===
+***
+
+## Image information
+- Base : Cent OS:latest
+- Volume
+    - /home
+- Port
+    - 22 : For ssh
+- Privileged : True (Require system D-Bus)
+- SSH base account 
+    - user : root
+    - pw : centos123
+***
+## How in use?
+1. Build container image
+
+```bash
+docker build -t (image name) .
+```
+
+2. Run container and execute essential shell script
+
+```
+docker run -d --privileged --name (container name) -v $(pwd)/(local mount directory):/home -p 9101:22 --restart=unless-stopped (image name) /sbin/init 
+&& 
+docker exec (container name) entrypoint.sh
+```
+***
---
+++ SSH_environment/centos/init.py
@@ -0,0 +1,33 @@
+import argparse,subprocess
+
+parser = argparse.ArgumentParser(
+    prog="Cent OS Docker SSH env",
+)
+
+parser.add_argument("-n",required=False,help="Docker Container Name | Not Required")
+parser.add_argument ("-v",required=False,help="Docker mount local volume | Not Required")
+parser.add_argument("-p",required=False,help="Docker port bind | Not Required")
+parser.add_argument("-i",required=True,help="Docker Image Name | Required")
+
+# Convert args to dictionary
+args = vars(parser.parse_args())
+
+command_dockerrun = "sudo docker run -d --privileged"
+option_dockerrun = list()
+
+
+# Option --name
+option_dockerrun.append(f"--name {args['n']}") if args['n'] else False
+# Option -v
+option_dockerrun.append(f"-v {args['v']}:/home") if args['v'] else False
+# Option -p
+option_dockerrun.append(f"-p {args['p']}:22") if args['p'] else False
+# Restart option
+option_dockerrun.append("--restart=unless-stopped")
+# Option i
+option_dockerrun.append(f"{args['i']}")
+# Starting command
+option_dockerrun.append("/sbin/init")
+command_dockerrun = f"{command_dockerrun} {' '.join(option_dockerrun)}"
+command_initiate = f"docker exec {args['n']} entrypoint.sh"
+subprocess.run(f"sudo {command_dockerrun} && {command_initiate}",shell=True)
